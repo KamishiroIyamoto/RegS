daeb43d5e6616e592f4a0378001d73b43493fcd2
Kamishiro Iyamoto <lipko03@mail.ru>
2021-11-22 01:46:34 +0300

    next lvl design

--- resources/js/scripts/sign.js
+++ resources/js/scripts/sign.js
@@ -2,11 +2,13 @@ document.forms.form.onsubmit = function(e){
     e.preventDefault();
     document.getElementById('password').value = encryptPassword(document.getElementById('password').value);
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost/RegS/core/index.php');
     var formData = new FormData(document.forms.form);
     xhr.onreadystatechange = function(){
         if(xhr.readyState === 4 && xhr.status === 200){
             console.log('So good!');
+        }
+        else{
+            console.log('Произошла ошибка');
         }
     }
     xhr.send(formData);
